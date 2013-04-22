f381e9d531c8f5f6a95ff3d71c36263474b5e21c
Christian Haslund <chr@haslundmedia.com>
2013-04-22 17:12:08 +0200

    New Menu icons larger added

--- src/kea/togkontrolloer/activities/SpottingOverviewActivity.java
+++ src/kea/togkontrolloer/activities/SpottingOverviewActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
