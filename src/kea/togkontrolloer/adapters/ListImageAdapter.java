f502fee5d24d3221650156c45ca17d6c0b405971
Casper Frimodt <casper.frimodt@gmail.com>
2013-04-22 16:20:30 +0200

    Updated adapter and layout for listImage

--- src/kea/togkontrolloer/adapters/ListImageAdapter.java
+++ src/kea/togkontrolloer/adapters/ListImageAdapter.java
@@ -2,23 +2,24 @@ package kea.togkontrolloer.adapters;
  
 import java.util.ArrayList;
 
-import android.R;
+import kea.togkontrolloer.R;
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 
 import android.widget.TextView;
  
-public class LazyAdapter extends BaseAdapter{
+public class ListImageAdapter extends BaseAdapter{
  
     private Activity activity;
     private ArrayList<CustomListView> data;
     private static LayoutInflater inflater=null;
  
-    public LazyAdapter(Activity a, ArrayList<CustomListView> d) {
+    public ListImageAdapter(Activity a, ArrayList<CustomListView> d) {
         this.activity = a;
         this.data=d;
         this.inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -39,18 +40,17 @@ public class LazyAdapter extends BaseAdapter{
     public View getView(int position, View convertView, ViewGroup parent) {
         View vi=convertView;
         if(convertView==null)
-            vi = inflater.inflate(R.layout.test_list_item, null);
+            vi = inflater.inflate(R.layout.list_row, null);
  
         TextView title = (TextView)vi.findViewById(R.id.title); // title
-        //TextView artist = (TextView)vi.findViewById(R.id.artist); // artist name
-        //TextView duration = (TextView)vi.findViewById(R.id.duration); // duration
-        //ImageView thumb_image=(ImageView)vi.findViewById(R.id.); // thumb image
- 
+        ImageView thumb_image=(ImageView)vi.findViewById(R.id.list_image);
         CustomListView item = new CustomListView();
         item = data.get(position);
  
         // Setting all values in listview
+        //title.setText(item.title);
         title.setText(item.title);
+        thumb_image.setImageResource(item.image);
         //imageLoader.DisplayImage(song.get(CustomizedListView.KEY_THUMB_URL), thumb_image);
         return vi;
     }
