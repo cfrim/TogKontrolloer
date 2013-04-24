10aaeb7ae6855ea65ca684fec1b32060163362b8
Niklas Markussen <njm@signifly.com>
2013-04-24 10:13:32 +0200

    Saving trainlines.json again

--- src/kea/togkontrolloer/helpers/RequestHelp.java
+++ src/kea/togkontrolloer/helpers/RequestHelp.java
@@ -344,10 +344,7 @@ public static boolean setFavoritesLocalJSON(String filename, String content){
 			
 			requestJSON = getServerJSON("http://cfrimodt.dk/test/ticket-dodger/?do=getLines&sec=314bf797090f40e9cbf54909b4814a4c1679cf4c2aae390559c15248a0055c12");
 			Request request = getRequest(requestJSON); 
-			
-			request.getOut();
-			
-			//setLocalJSON(filenameTrainLines, requestJSON);
+			setLocalJSON(filenameTrainLines, requestJSON);
 			
 		}else{
 			
